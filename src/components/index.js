6938a97f0fc7c9d6dae4da99f7f05d02a816bf3d
HappySpitz <tatarinkristina2002@gmail.com>
2023-01-24 18:34:07 +0200

    index fix

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,9 @@ export * from './Post/Post';
 
 export * from './Users/Users';
 export * from './User/User';
+
+export * from './UserPosts/UserPosts';
+export * from './UserPost/UserPost';
+
+export * from './Launches/Launches';
+export * from './Launch/Launch';
