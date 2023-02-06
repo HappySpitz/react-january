befbb693aa1dc40d59c20c939b0c06113220b32c
HappySpitz <tatarinkristina2002@gmail.com>
2023-02-06 18:01:58 +0200

    redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import {useDispatch, useSelector} from "react-redux";
+
 const App = () => {
-  return (
-    <div>
-    </div>
+
+  let state = useSelector((state) => state);
+  let dispatch = useDispatch();
+
+    return (
+        <div>
+            <h2>{state}</h2>
+            <button onClick={() => dispatch({type: "INC", payload: 1})}>increment</button>
+            <button onClick={() => dispatch({type: "DEC", payload: 1})}>decrement</button>
+        </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {App} from './App';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+
+function reducer(state= 0, action) {
+    switch (action.type) {
+        case "INC":
+            state = state + action.payload;
+            return state;
+        case "DEC":
+            state = state - action.payload
+            return state;
+        default:
+            return state;
+    }
+}
+
+let store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
